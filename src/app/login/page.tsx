12757f52137be21a1fa7e32cb22f628d1a910be0
Nikita Zaginey <nikitazaginey1@gmail.com>
2023-09-20 10:36:00 +0200

    create pages

---
+++ src/app/login/page.tsx
@@ -0,0 +1,7 @@
+export default function LoginPage() {
+  return (
+    <div className="flex">
+      <h1 className="text-center text-2xl">LogIn</h1>
+    </div>
+  );
+}
---
+++ src/app/signup/page.tsx
@@ -0,0 +1,8 @@
+export default function SingupPage() {
+    return (
+      <div className="flex">
+        <h1 className="text-center text-2xl">SignUp</h1>
+      </div>
+    );
+  }
+  
