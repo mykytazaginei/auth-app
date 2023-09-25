6a5e01e2274c2b8a4d5f34a0a653d4e456bff24d
Nikita Zaginey <nikitazaginey1@gmail.com>
2023-09-25 16:31:33 +0200

    login api

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,18 +2,45 @@
 import Link from "next/link";
 import React from "react";
 import { useRouter } from "next/navigation";
-import { Axios } from "axios";
+import axios from "axios";
+import { toast } from "react-hot-toast";
 
 export default function LoginPage() {
+  const router = useRouter();
   const [user, setUser] = React.useState({
     email: "",
     password: "",
   });
-  const onLogin = async () => {};
+
+  const [buttonDisable, setButtonDisable] = React.useState(false);
+
+  const [loading, setLoading] = React.useState(false);
+
+  const onLogin = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.post("/api/users/login", user);
+      console.log("Login success", response.data);
+      toast.success("Login success");
+      router.push("/profile");
+    } catch (error: any) {
+      console.log("Login failed", error.message);
+      toast.error(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+  React.useEffect(() => {
+    if (user.email.length > 0 && user.password.length > 0) {
+      setButtonDisable(false);
+    } else {
+      setButtonDisable(true);
+    }
+  }, [user]);
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <h1>LogIn</h1>
+      <h1>{loading ? "Processing" : "Login"}</h1>
       <hr />
       <label htmlFor="email">Email</label>
       <input
@@ -37,9 +64,9 @@ export default function LoginPage() {
         onClick={onLogin}
         className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
       >
-        LogIn
+        {buttonDisable ? " No Login" : "Login"}
       </button>
-      <Link href='/signup'>Visit SignUp page</Link>
+      <Link href="/signup">Visit SignUp page</Link>
     </div>
   );
 }
