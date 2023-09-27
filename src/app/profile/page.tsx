3f780d798f8d18edf7fb4268ddbf9844805cbcea
Nikita Zaginey <nikitazaginey1@gmail.com>
2023-09-27 10:48:32 +0200

    logout logic

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,10 +1,34 @@
+"use client";
+import Link from "next/link";
+import React from "react";
+import { useRouter } from "next/navigation";
+import axios from "axios";
+import { toast } from "react-hot-toast";
+
 export default function ProfilePage() {
+  const router = useRouter();
+  const logout = async () => {
+    try {
+      await axios.get("/api/users/logout");
+      toast.success("Logout successful");
+      router.push("/login");
+    } catch (error: any) {
+      console.log(error.message);
+      toast.error(error.message);
+    }
+  };
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
-        <h1>Profile</h1>
-        <hr/>
-        <p>Profile page</p>
-        
+      <h1>Profile</h1>
+      <hr />
+      <p>Profile page</p>
+      <hr />
+      <button
+        onClick={logout}
+        className="bg-blue-500 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      >
+        Logout
+      </button>
     </div>
   );
 }
