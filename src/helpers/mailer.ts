9b2d6eb104385a7e7c41263c47d7a16578841812
Nikita Zaginey <nikitazaginey1@gmail.com>
2023-10-01 13:19:25 +0200

    add a mailer.ts for mail sender

---
+++ src/helpers/mailer.ts
@@ -0,0 +1,48 @@
+import nodemailer from "nodemailer";
+import User from "@/models/userModel";
+import bycryptjs from "bcryptjs";
+
+export const sendEmail = async ({ email, emailType, userId }: any) => {
+  try {
+    //create a hashed token
+    const hashedToken = await bycryptjs.hash(userId.toString(), 10);
+
+    if (emailType === "VERIFY") {
+      await User.findByIdAndUpdate(userId, {
+        verifyToken: hashedToken,
+        verifyTokenExpiry: Date.now() + 3600000,
+      });
+    } else if (email === "RESET") {
+      await User.findByIdAndUpdate(userId, {
+        forgotPassworToken: hashedToken,
+        forgotPAsswordtokenExpiry: Date.now() + 3600000,
+      });
+    }
+
+    var transport = nodemailer.createTransport({
+      host: "sandbox.smtp.mailtrap.io",
+      port: 2525,
+      auth: {
+        user: process.env.TRANSPORT_USER,
+        pass: process.env.TRANSPORT_PASS,
+      },
+    });
+
+    const mailOptions = {
+      from: "nikitazaginey1@gmail.com",
+      to: email,
+      subject:
+        emailType === "VERIFY" ? "verify your email" : "Reset your password",
+      html: `<p>Click <a href="${
+        process.env.DOMAIN
+      }/verifyemail?token=${hashedToken}">here</a> to ${
+        emailType === "VERIFY" ? "verify your email" : "reset ypur password"
+      }</p>`,
+    };
+
+    const mailResponse = await transport.sendMail(mailOptions);
+    return mailResponse;
+  } catch (error: any) {
+    throw new Error(error.message);
+  }
+};
